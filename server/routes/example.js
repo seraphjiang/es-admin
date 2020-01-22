31457450fffdbcec6d3e8dd285474f10d60aa6eb
huanji <huanji@amazon.com>
2020-01-22 14:38:13 -0800

    save index

--- server/routes/example.js
+++ server/routes/example.js
@@ -1,10 +1,24 @@
 export default function (server) {
+  // const { rest } = server.config().get('elasticsearch');
+  let { callWithRequest } = server.plugins.elasticsearch.getCluster('data');
 
   // http://localhost:5601/api/es-admin/example
   server.route({
     path: '/api/es-admin/example',
     method: 'GET',
     handler() {
+      // GET _cat/indices?v&s=index:asc
+      
+      callWithRequest({}, 'index', {
+        index: 'my-index',
+        type: 'documents',
+        id: 'document_12345',
+        body: {
+          fieldA: 'something',
+          fieldB: 'awesome'
+        }
+      });
+
       return { time: (new Date()).toISOString() };
     }
   });
