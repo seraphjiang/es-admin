8491990ca08cdc203e0484064601efebf9eeddb1
huanji <huanji@amazon.com>
2020-01-22 14:47:02 -0800

    add basic delete function

--- server/routes/example.js
+++ server/routes/example.js
@@ -8,16 +8,49 @@ export default function (server) {
     method: 'GET',
     handler() {
       // GET _cat/indices?v&s=index:asc
-      
+      // GET my-index/_search
       callWithRequest({}, 'index', {
         index: 'my-index',
         type: 'documents',
         id: 'document_12345',
         body: {
           fieldA: 'something',
-          fieldB: 'awesome'
+          fieldB: 'awesome',
+          date: Date.now()
         }
       });
+
+
+      callWithRequest({}, 'index', {
+        index: 'my-index',
+        type: 'documents',
+        id: 'document_2',
+        body: {
+          fieldA: 'something',
+          fieldB: 'awesome',
+          date: Date.now()
+        }
+      });
+      
+      callWithRequest({}, 'index', {
+        index: 'my-index',
+        type: 'documents',
+        id: 'document_3',
+        body: {
+          fieldA: 'something',
+          fieldB: 'awesome',
+          date: Date.now()
+        }
+      }).then(res => {
+        console.log('insert doc 3')
+        callWithRequest({}, 'delete', {
+          index: 'my-index',
+          type: 'documents',
+          id: 'document_3'
+        }).then(re => {
+          console.log('delete doc 3')
+        })
+      })
 
       return { time: (new Date()).toISOString() };
     }
